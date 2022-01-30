8b6ba2e585625b73f228105b7524aeeb29c1a3bd
NeerajSingh511997 <NeerajSingh511997@gmail.com>
2022-01-30 12:27:29 +0530

    added admin table schema

--- app/admin.py
+++ app/admin.py
@@ -1,12 +1,31 @@
+from csv import list_dialects
 from curses.ascii import US
 from django.contrib import admin
 # from django.apps import apps
 from .models import *
 
 
-# models = apps.get_models()
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'published', 'price', 'address', 'city', 'state', 'created')
+    list_link = ('id', 'title')
+    list_filter = ('city', 'state')
+    list_editable = ('published',)
+    list_per_page = 25
+    list_max_show_all = 50
+    search_fields = ('title', 'address', 'city', 'state', 'address')
 
-# Register your models here.
-admin.site.register(Listing)
-admin.site.register(Realtor)
+    
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'top_seller', 'created')
+    list_link = ('id', 'name', 'email')
+    list_filter = ('top_seller',)
+    search_fields = ('id', 'name', 'email')
+    list_per_page = 25
+    list_max_show_all = 50
+    list_editable = ('top_seller',)
+
+
+# REGISTER MODEL AND ADMIN SCHEMA;
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(Realtor, RealtorAdmin)
 admin.site.register(User)
